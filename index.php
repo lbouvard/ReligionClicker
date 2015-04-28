aef368e9f2e8e2f8b574efd31e98e206c5389db1
lbouvard <lbouvard57@gmail.com>
2015-04-28 04:29:38 -0700

    Mise en place du layout.

--- index.php
+++ index.php
@@ -1,3 +1,61 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Religion Clicker</title>
+
+    <!-- Bootstrap -->
+	<link href="css/layout.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  
+  <body>
+	<div class="container-fluid">
+	
+		<div class="row">
+			<div class="col-xs-12 col-md-4">
+				<h3>Partie clicker</h3>
+				
+				<p>
+					<button type="button" class="btn btn-primary btn-lg">Large button</button>
+				</p>
+			</div>
+			
+			<div class="col-md-5">
+				<h3>Partie centrale</h3>
+			</div>
+			
+			<div class="col-md-3">
+				<h3>Partie shop</h3>
+			</div>
+		</div>
+		
+	</div>
+
+	<footer class="footer">
+		<div class="container">
+			<p class="text-muted">Team GOFRBO.</p>
+		</div>
+    </footer>
+	
+    <!-- jQuery -->
+    <script src="js/jquery-1.11-2.min.js"></script>
+    <!-- bootstrap -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+  
+</html>
+
 <?php
 // wamp\www\ReligionClicker\index.php
 
